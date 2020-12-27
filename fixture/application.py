e7e5b33a5622e13f962c84d336ae28592ecc7b9c
l0my <30499048+l0my@users.noreply.github.com>
2020-12-27 22:54:35 +0300

    Created new class for the session manage.

--- fixture/application.py
+++ fixture/application.py
@@ -1,26 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
-from contacts import Contact
-
+from fixture.session import SessionHelper
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def return_to_groups_page(self):
         wd = self.wd
@@ -138,17 +129,6 @@ class Application:
     def open_groups_page(self):
         wd = self.wd
         wd.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,7 +12,7 @@ def app(request):
 
 
 def test_add_test_group(app):
-    app.login("admin", "secret")
+    app.session.login("admin", "secret")
     app.create_contact(Contact(firstname="Anatolii", middlename="Vladimirovich", lastname="Trofimov",
                                 nickname="l0my", title="titles", company="bercut", address="saint-petersburg",
                                 home="sweet home", mobile="85945739", work="32302", fax="445566",
@@ -20,4 +20,4 @@ def test_add_test_group(app):
                                 email2="epwrojwe@gmail.com", email3="qwpejqwewq@ya.ru", homepage="my home page",
                                 bday="15", bmonth="January", byear="1884", aday="18", amonth="October",
                                 ayear="1994", group_name="assdfdsf", address2="greg", phone2="rwgw", notes="qwe"))
-    app.logout()
+    app.session.logout()
