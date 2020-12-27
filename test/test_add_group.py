062aaadcdd270b31a492b92346708577f822b584
l0my <30499048+l0my@users.noreply.github.com>
2020-12-27 23:33:50 +0300

    Created new classes for the navigation, contact and groups.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="aofeoifeq", header="pijfwpfe", footer="qpfqj[eofq"))
+    app.group.create(Group(name="aofeoifeq", header="pijfwpfe", footer="qpfqj[eofq"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
