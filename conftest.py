742e30fc8b943dcb407e1444218a01f7e7a0a6a9
l0my <30499048+l0my@users.noreply.github.com>
2020-12-29 22:35:07 +0300

    Created new class for the delete first group. Replace fixture to file conftest. Added scope to the fixture.

---
+++ conftest.py
@@ -0,0 +1,10 @@
+from contacts import Contact
+from fixture.application import Application
+import pytest
+
+
+@pytest.fixture(scope='session')
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
 from contacts import Contact
-from fixture.application import Application
-import pytest
-
-
-@pytest.fixture()
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
