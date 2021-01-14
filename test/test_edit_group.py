76507fdbe5c973b4136b1da21773bc1e5d1d89af
l0my <30499048+l0my@users.noreply.github.com>
2021-01-14 13:34:48 +0300

    Added some checks with groups.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,16 +4,25 @@ from model.group import Group
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit(Group(name="new name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit(Group(header="new header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit(Group(footer="new footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
