63b31fc6af80dafec0499a19b99f48bcc07081c5
l0my <30499048+l0my@users.noreply.github.com>
2021-01-21 17:49:57 +0300

    Changed tes with adding a new group.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,11 +9,9 @@ def random_string(prefix, maxlen):
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-test_data = [Group(name=name, header=header, footer=footer)
-        for name in ["", random_string("name", 10)]
-        for header in ["", random_string("header", 10)]
-        for footer in ["", random_string("footer", 10)]
-        ]
+test_data = [Group(name="", header="", footer="")] +\
+            [Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
+             for i in range(5)]
 
 
 @pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
