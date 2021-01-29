e57a141230039a493e7f10adf27dce85d914eabd
l0my <30499048+l0my@users.noreply.github.com>
2021-01-29 19:27:11 +0300

    Changed tests for loading data from the DB instead of UI.

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,9 +1,13 @@
 from model.group import Group
+from timeit import timeit
 
 
 def test_group_list(app, db):
-    ui_list = app.group.get_group_list()
+    print(timeit(lambda: app.group.get_group_list(), number=1))
+    # ui_list = app.group.get_group_list()
     def clean(group):
         return Group(id=group.id, name=group.name.strip())
-    db_list = map(clean, db.get_group_list())
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    # db_list = map(clean, db.get_group_list())
+    print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
+    assert False
+    # assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
